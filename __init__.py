998660a3645b62b4798458e2e9772d53cc14604a
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2018-01-19 15:04:33 +0000

    save brain

--- __init__.py
+++ __init__.py
@@ -50,8 +50,7 @@ class AimlFallback(AutotranslatableFallback):
                 except Exception as e:
                     self.log.error(e)
             try:
-                pass
-                #self.kernel.saveBrain(self.brain_path)
+                self.kernel.saveBrain(self.brain_path)
             except Exception as e:
                 self.log.error(e)
 
